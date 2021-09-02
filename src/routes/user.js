b369b6f06a0be0bc1a3e8ab9977f54eccc2d685e
Rodrigo Beuzeville <robeuzeville@gmail.com>
2021-09-01 22:11:53 -0500

    Cambios

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,11 +1,13 @@
 const { Router } = require('express');
 const {} = require("../middlewares/auth")
-const { /*getProfile*/ } = require('../controllers/user');
+const { crearUsuario } = require('../controllers/user');
 const router = Router();
 
 /*
   EXAMPLE:
   router.get('/', nameAuth, getProfile)
 */
+
+router.get('/', crearUsuario)
 
 module.exports = router;
