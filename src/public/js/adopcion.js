4b395bc2e106f86d562e7573e7fde95cc94f6e48
NyoByte <20173527@aloe.ulima.edu.pe>
2021-09-03 23:10:49 -0500

    Header y Footer cambiados a Partials y Formulario de adopción avanzado al 40%

--- src/public/js/adopcion.js
+++ src/public/js/adopcion.js
@@ -1,5 +1,13 @@
+function CambiarHeader(){
+    for (var i = 0; i <4; i++){
+        document.getElementsByClassName("nav-link").item(i).setAttribute("class", "nav-link")
+    }
+    document.getElementsByClassName("nav-link").item(2).setAttribute("class", "nav-link active")
+    
+}
 var main = function(){
     $('#avisoAdopcionModal').modal({ show: true, backdrop: 'static', keyboard: false})
+    CambiarHeader();
 }
 
 window.addEventListener("load", main)
