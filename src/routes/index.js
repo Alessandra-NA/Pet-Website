ebaf4eb9352af6f5bf8841318aedb562da60e869
Ale <alessandra.na001@gmail.com>
2021-09-06 09:25:01 -0500

    Added Match form

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,6 +3,7 @@ const express = require('express');
 const { mainPage } = require('../controllers');
 const userRoutes = require('./user');
 const adopcionRoutes = require('./adopcion')
+const matchRoutes = require('./match')
 
 /**
   * @param {express.Express} app
@@ -12,6 +13,7 @@ const routerConnection = (app) => {
   app.use('^/$', mainPage);
   app.use('/user', userRoutes);
   app.use('/adopcion', adopcionRoutes);
+  app.use('/match', matchRoutes);
 
   app.use((_, res) => {
     res.render('notFound');
