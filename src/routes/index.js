cf9ee74b15655fa1466a8e26dda6bab551f6d926
Ale <alessandra.na001@gmail.com>
2021-09-06 19:13:47 -0500

    testing

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,6 +3,7 @@ const express = require('express');
 const { mainPage } = require('../controllers');
 const userRoutes = require('./user');
 const adopcionRoutes = require('./adopcion')
+const anunciosRouter = require('./anuncios')
 
 /**
   * @param {express.Express} app
@@ -12,6 +13,7 @@ const routerConnection = (app) => {
   app.use('^/$', mainPage);
   app.use('/user', userRoutes);
   app.use('/adopcion', adopcionRoutes);
+  app.use('/anuncios', anunciosRouter);
 
   app.use((_, res) => {
     res.render('notFound');
