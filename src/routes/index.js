a9887342a0b5b5049eab08e7b5c1c2e0ac981711
NyoByte <20173527@aloe.ulima.edu.pe>
2021-09-08 09:27:27 -0500

    Done layout de cards para los anuncios de adopcion

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,7 +3,10 @@ const express = require('express');
 const { mainPage } = require('../controllers');
 const userRoutes = require('./user');
 const adopcionRoutes = require('./adopcion')
+<<<<<<< Updated upstream
 const matchRoutes = require('./match')
+=======
+>>>>>>> Stashed changes
 const anunciosRouter = require('./anuncios')
 
 /**
@@ -14,7 +17,10 @@ const routerConnection = (app) => {
   app.use('^/$', mainPage);
   app.use('/user', userRoutes);
   app.use('/adopcion', adopcionRoutes);
+<<<<<<< Updated upstream
   app.use('/match', matchRoutes);
+=======
+>>>>>>> Stashed changes
   app.use('/anuncios', anunciosRouter);
 
   app.use((_, res) => {
