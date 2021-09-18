85ac62ab7250a1a06ffa4912f0837d82f1feb7b0
vigacv <vgabrielcv@gmail.com>
2021-09-17 20:53:51 -0500

    Gestion de cuentas done

---
+++ src/routes/accounts.js
@@ -0,0 +1,14 @@
+const { Router } = require('express');
+const {} = require("../middlewares/auth")
+const { showAccounts, deleteAccount } = require('../controllers/accounts');
+const router = Router();
+
+
+/*
+  EXAMPLE:
+  router.get('/', nameAuth, getPetPost)
+*/
+router.get('/', showAccounts)
+router.get('/delete/:userid', deleteAccount)
+
+module.exports = router;
