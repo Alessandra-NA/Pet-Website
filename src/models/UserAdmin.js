ac0e9159595208235728e84dee690f46a3cc192e
Alessandra Nuñez <64923812+Alessandra-NA@users.noreply.github.com>
2021-09-22 12:41:54 -0500

    Edit models (#17)
    * modelos corregidos

---
+++ src/models/UserAdmin.js
@@ -0,0 +1,21 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../db');
+
+class UserAdmin extends Model {
+    // relaciones:
+    static associate({ User }) {
+        this.belongsTo(User, { as: 'user', foreignKey: 'user_id' });
+    }
+}
+UserAdmin.init(
+    {
+        // atributos:
+
+    },
+    {
+        sequelize,
+        modelName: 'UserAdmin',
+    }
+);
+
+module.exports = { UserAdmin };
