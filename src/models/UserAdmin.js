f80b4bcc32598578b0d409743f64dc569c67fbcf
Ale <alessandra.na001@gmail.com>
2021-09-22 12:39:35 -0500

    modelos corregidos

--- src/models/UserAdmin.js
+++ src/models/UserAdmin.js
@@ -3,15 +3,14 @@ const sequelize = require('../db');
 
 class UserAdmin extends Model {
     // relaciones:
-    static associate({ User, Admin }) {
+    static associate({ User }) {
         this.belongsTo(User, { as: 'user', foreignKey: 'user_id' });
-        this.belongsTo(Admin, { as: 'admin', foreignKey: 'admin_id' });
     }
 }
 UserAdmin.init(
     {
         // atributos:
-        
+
     },
     {
         sequelize,
