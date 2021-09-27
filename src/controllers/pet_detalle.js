482f9e0a6c5e08a7ae5235caac87b97f655abda9
Willy Four <52677485+HotIce803@users.noreply.github.com>
2021-09-26 19:37:01 -0500

    Will (#19)
    * Vista Shelters
    
    * Shelters y shelter detalle
    
    * Update package.json

---
+++ src/controllers/pet_detalle.js
@@ -0,0 +1,16 @@
+const { } = require('../models');
+
+module.exports = {
+  /**
+   * @param {import('express').Request} req
+   * @param {import('express').Response} res
+   *
+   */
+
+  getInicioPetDetalle: async (req, res) => {
+    try{
+      return res.render('pet_detalle',{title:'Detalle de mascota'});
+    }
+    catch{}
+  },
+};
