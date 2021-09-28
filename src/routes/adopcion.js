993aabf0befd3a25aa053ec4406b230a3ad13200
NyoByte <56806822+NyoByte@users.noreply.github.com>
2021-09-28 14:28:15 -0500

    Nyo (#22)
    * Pasar imagen (error, pasa en null)
    
    * Revisando subida de imagen
    
    * Subido imágenes de adopción DONE
    
    * Eliminar submit test

--- src/routes/adopcion.js
+++ src/routes/adopcion.js
@@ -2,13 +2,16 @@ const { Router } = require('express');
 const {} = require("../middlewares/auth")
 const { getInicioAdopcion, postAdopcion } = require('../controllers/adopcion');
 const router = Router();
+const multer = require('multer');
+const {Pet, Post} = require('../models');
 
+upload = multer();
 
 /*
   EXAMPLE:
   router.get('/', nameAuth, getPetPost)
 */
 router.get('/', getInicioAdopcion)
-router.post('/postAdopcion', postAdopcion);
+router.post('/postAdopcion', upload.single('image'), postAdopcion);
 
 module.exports = router;
