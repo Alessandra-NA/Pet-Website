290bbf6062cc1747f17803d32b45189c26aee726
vigacv <vgabrielcv@gmail.com>
2021-10-03 12:36:30 -0500

    Added authentication for account admin

--- src/routes/accounts.js
+++ src/routes/accounts.js
@@ -1,11 +1,6 @@
 const { Router } = require('express');
-<<<<<<< HEAD
-const {} = require("../middlewares/auth")
-const { showAccounts, deleteAccount, showAccountDetails } = require('../controllers/accounts');
-=======
 const { authAdmin } = require("../middlewares/auth")
 const { showAccounts, deleteAccount } = require('../controllers/accounts');
->>>>>>> main
 const router = Router();
 
 
@@ -13,7 +8,7 @@ const router = Router();
   EXAMPLE:
   router.get('/', nameAuth, getPetPost)
 */
-router.get('/', authAdmin, showAccounts)
+router.get('/', showAccounts)
 router.get('/delete/:userid', deleteAccount)
 
 module.exports = router;
