d3ef13f7ef76b0dfe5a11b3f8a43f1aafa46572e
vigacv <vgabrielcv@gmail.com>
2021-10-23 16:14:48 -0500

    Hecho formulario nuevo establecimiento

--- src/controllers/establishment.js
+++ src/controllers/establishment.js
@@ -17,7 +17,7 @@ module.exports = {
                 ]
             })
             console.log(establishments)
-            return res.render('establecimientos', { title: 'Establecimientos', establecimientos: imagesToBase64ForEstablishment(establishments), listado: req.query.tab!='mapa'});
+            return res.render('establecimientos', { title: 'Establecimientos', establecimientos: imagesToBase64ForEstablishment(establishments), listado: req.query.tab != 'mapa' });
         }
         catch (err) {
             console.log(err);
@@ -26,10 +26,43 @@ module.exports = {
 
     showFormNew: async (req, res) => {
         try {
-            return res.render('new_establishment');
+            return res.render('new_establishment', { lat: req.query.lat, long: req.query.long });
         }
         catch (err) {
             console.log(err);
+        }
+    },
+
+    saveNewEstablishment: async (req, res) => {
+        try {
+            const { name, type, address, mapLat, mapLng, province, district, ofPets, link } = req.body
+            const photo = req.file ? req.file.buffer : null
+
+            const newLocation = Location.create({
+                country: 'Perú',
+                province: province,
+                district: district,
+                address: address
+            })
+
+            newLocation.then(data => {
+                Establishment.create({
+                    name: name,
+                    photo: [photo],
+                    rating: 0,
+                    ofPets: ofPets ? true : false,
+                    type: type,
+                    link: link,
+                    latitude: parseFloat(mapLat),
+                    longitude: parseFloat(mapLng),
+                    location_id: data.id,
+                })
+                .then(data => console.log("Establecimiento creado correctamente"), 
+                err => console.error(err))
+            })
+            res.redirect('/establishments?tab=mapa')
+        } catch (error) {
+            console.error(error)
         }
     }
 }
@@ -37,7 +70,7 @@ module.exports = {
 imagesToBase64ForEstablishment = function (establishmentArray) {
     let array = establishmentArray.map((establishment) => {
         if (establishment.photo) {
-            for(var i=0; i<establishment.photo.length; i++) {
+            for (var i = 0; i < establishment.photo.length; i++) {
                 establishment.photo[i] = Buffer.from(establishment.photo[i]).toString('base64');
             }
         }
--- src/routes/establishment.js
+++ src/routes/establishment.js
@@ -1,7 +1,10 @@
 const { Router } = require('express');
 const {} = require("../middlewares/auth")
-const { getEstablishments, showFormNew } = require('../controllers/establishment');
+const { getEstablishments, showFormNew, saveNewEstablishment } = require('../controllers/establishment');
 const router = Router();
+const multer = require('multer');
+
+upload = multer();
 
 
 /*
@@ -10,5 +13,6 @@ const router = Router();
 */
 router.get('/', getEstablishments)
 router.get('/new', showFormNew)
+router.post('/new/save', upload.single('image'), saveNewEstablishment)
 
 module.exports = router;
