7c1587f0cd78f2261767bfad1792bca31a2986f1
NyoByte <20173527@aloe.ulima.edu.pe>
2021-10-26 14:42:04 -0500

    Database updated

--- src/models/Suggestion.js
+++ src/models/Suggestion.js
@@ -3,14 +3,18 @@ const sequelize = require('../db');
 
 class Suggestion extends Model{
     // relaciones:
-    static associate({ Establishment, Location }) {
-        this.belongsTo(Location, { as: 'location', foreignKey: 'location_id' });
-        this.belongsTo(Establishment,{as: 'establishment', foreignKey: 'establishment_id'});
+    static associate({ Establishment, User }) {
+        this.belongsTo(User, { as: 'user', foreignKey: {allowNull: false, name: 'user_id'}, onDelete: 'cascade'});
+        this.belongsTo(Establishment, { as: 'establishment', foreignKey: 'establishment_id' });
     }
 }
 Suggestion.init(
     {
         // atributos:
+        fecha: {
+            type: DataTypes.DATEONLY,
+            allowNull: false,
+        },
         name: {
             type: DataTypes.STRING(100),
             allowNull: true,
@@ -19,16 +23,20 @@ Suggestion.init(
             type: DataTypes.ARRAY(DataTypes.BLOB),
             allowNull: true,
         },
-        rating: {
-            type: DataTypes.INTEGER,
-            allowNull: true,
-        },
         type: {
             type: DataTypes.STRING(50),
             allowNull: true,
         },
         link : {
             type: DataTypes.STRING(150),
+            allowNull: true,
+        },
+        district: {
+            type: DataTypes.STRING(50),
+            allowNull: true,
+        },
+        address: {
+            type: DataTypes.STRING(100),
             allowNull: true,
         },
     },
