bb36c50084d753d8b5bcc5b03023582da8789388
vigacv <vgabrielcv@gmail.com>
2021-10-27 21:55:12 -0500

    Correcciones

--- src/routes/index.js
+++ src/routes/index.js
@@ -16,7 +16,6 @@ const signinRouter = require('./signin')
 const postRouter = require('./post')
 const accountsRouter = require('./accounts')
 const establishmentRouter = require('./establishment')
-const newEstablishmentRouter = require('./new_establishment')
 const establcimientoDetalleRouter = require('./establecimiento_detalle')
 const suggestionFormRouter = require('./suggestion_form')
 /**
@@ -43,7 +42,6 @@ const routerConnection = (app) => {
   app.use('/accounts', accountsRouter);
 
   app.use('/establishments', establishmentRouter)
-  app.use('/establishments/new', newEstablishmentRouter)
   app.use('/establecimiento_detalle', establcimientoDetalleRouter)
   app.use('/establecimiento_detalle/ds', establcimientoDetalleRouter)
   app.use('/suggestion_form', suggestionFormRouter)
