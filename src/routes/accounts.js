e132f05ef5a0f9ec787d6decb0b50d7ce68938fa
Willy Fournier Claros <proveedorwilly.fournier@sura.pe>
2021-11-21 13:47:37 -0500

    Administrar Comentarios Usuario

--- src/routes/accounts.js
+++ src/routes/accounts.js
@@ -1,6 +1,6 @@
 const { Router } = require('express');
 const { authAdmin } = require("../middlewares/auth")
-const { showAccounts, deleteAccount, showAccountDetails, chooseTypeAccountToManage, getAccounts, deleteEstablecimiento, getSugerencias, confirmarSugerencia, eliminarSugerengia, getReportesAdopcion, confirmarReporte } = require('../controllers/accounts');
+const { showAccounts, deleteAccount, showAccountDetails, chooseTypeAccountToManage, confirmarReporteComment, getAccounts, deleteEstablecimiento, getSugerencias, confirmarSugerencia, eliminarSugerengia, getReportesAdopcion, confirmarReporte, getReportesComentarios } = require('../controllers/accounts');
 const router = Router();
 
 
@@ -15,7 +15,9 @@ router.post('/confirmarSugerencia', confirmarSugerencia)
 router.get('/eliminarSugerencia/:sugid', authAdmin,eliminarSugerengia)
 //router.get('/', authAdmin, showAccounts) --> reemplazado por getAccounts
 router.get('/reporteAdopcion', authAdmin, getReportesAdopcion)
+router.get('/reporteComentario', authAdmin, getReportesComentarios)
 router.post('/cambiarStatus', confirmarReporte)
+router.post('/cambiarStatus2', confirmarReporteComment)
 router.get('/deleteEstablecimiento/:estid', authAdmin, deleteEstablecimiento)
 router.get('/delete/:userid', authAdmin, deleteAccount)
 
